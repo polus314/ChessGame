e2bccb86caf800e39f68e88c7c623a9079697feb
John Polus <johnpolus1@gmail.com>
2018-10-28 02:33:20 -0500

    Trying to track down why my AI is so stupid. Removing some unused classes and methods from AI.

---
+++ src/chessgame/ForceMateAI.java
@@ -0,0 +1,10 @@
+package chessgame;
+
+/**
+ * This AI is used for determining if it is possible and how to force the
+ * opposing player into checkmate.
+ * @author John
+ */
+public class ForceMateAI extends AI {
+    
+}
